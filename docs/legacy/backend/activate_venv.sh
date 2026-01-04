459b452c98b29f6b758ad8f11fc3fc9f36c42795
mptyl <mp@tylconsulting.it>
2026-01-04 14:47:49 +0100

    documentation review

--- docs/legacy/backend/activate_venv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script to activate the Python virtual environment
-source .venv/bin/activate
-echo "Virtual environment activated!"
-echo "Python executable: $(which python)"
-echo "Python version: $(python --version)"
-echo ""
-echo "To deactivate, type: deactivate"
